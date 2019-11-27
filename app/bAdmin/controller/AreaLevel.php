2a7026e1fa7d9e57ba68fba4aab0f8f94bb71cc6
FK <18310029587@163.com>
2019-11-27 14:36:11 +0800

    地区完成

--- app/bAdmin/controller/AreaLevel.php
+++ app/bAdmin/controller/AreaLevel.php
@@ -11,20 +11,79 @@ namespace app\bAdmin\controller;
 
 
 use app\common\service\Area;
+use app\common\service\Category;
+use app\common\typeCode\cate\LevelOption;
+use app\Request;
 use think\facade\View;
+use think\Validate;
 
 class AreaLevel extends Base
 {
-    public function index()
+    public function index(Request $request)
     {
         try{
+            $pid = $request->param('pid')?$request->param('pid'):0;
+
             //查询全部的省
-            $pidResult = (new Area())->getListByPId(0,true);
+            $pidResult = (new Area())->getListByPId($pid,15);
+            if($pidResult[0]['level']==3){
+                //单独查询一下上一级的id
+                $getFindResult = (new Area())->getFindById($pidResult[0]['pid']);
+                View::assign('threePid',$getFindResult['pid']);
+            }
+
+
             View::assign('pidResult',$pidResult);
 
             return view('area_level/index');
         }catch (\Exception $e){
             return $e->getMessage();
+        }
+    }
+
+    public function edit(Request $request)
+    {
+        try {
+            //查询城市
+            $city_id = $request->param('city_id');
+            $getFindResult = (new Area())->getFindById($city_id);
+            //查询级别选项的
+            $cateList = (new Category())->getList(new LevelOption());
+
+            View::assign('cateList',$cateList);
+            View::assign('data', $getFindResult);
+            return view();
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try{
+            $post = $request->post();
+
+            $valiatde = new Validate();
+            $valiatde->rule(Array(
+                'id'       => 'require',
+                'level_id|级别'=>'require',
+                'level_sort|排序'=>'require|between:0,999',
+                'is_hot|热门'=>'require',
+                '__token__'     => 'token',
+            ));
+
+            if(!$valiatde->check($post))  throw new \Exception($valiatde->getError());
+
+            //根据级别id查出值
+            $cateOneResult = (new Category())->getOneById($post['level_id']);
+
+            $post['level_value'] = $cateOneResult['name'];
+
+            $result = (new Area())->update($post);
+            if(!$result) throw new \Exception('修改失败');
+            return json_encode(['code'=>1,'msg'=>'success']);
+        }catch (\Exception $e){
+            return json_encode(['code'=>0,'msg'=>$e->getMessage()]);
         }
     }
 }
--- app/common/service/Area.php
+++ app/common/service/Area.php
@@ -11,7 +11,6 @@ namespace app\common\service;
 
 class Area
 {
-    private $pageLength = 15;
 
     /**
      * @param int $pId  父级id
@@ -21,13 +20,45 @@ class Area
      * @throws \think\db\exception\DbException
      * @throws \think\db\exception\ModelNotFoundException
      */
-    public function getListByPId($pId = 0,$page = false)
+    public function getListByPId($pId = 0,$page = Null)
     {
         $areaModel = new \app\common\model\Area();
 
         $handler = $areaModel->where(['pid'=>$pId]);
 
-        return $page ? $handler->paginate($this->pageLength) : $handler->select();
+        return $page ? $handler->paginate($page) : $handler->select();
 
+    }
+
+    /**
+     * 查询一条
+     * @param $id
+     * @return array|null|\think\Model
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     * $data 2019/11/27 13:53
+     */
+    public function getFindById($id)
+    {
+        return (new \app\common\model\Area())->get($id);
+    }
+
+    /**
+     * 更新数据
+     * @param $data
+     * @return \app\common\model\Area
+     * $data 2019/11/27 14:00
+     */
+    public function update($data)
+    {
+        $updateArr = [
+            'level_id' => $data['level_id'],
+            'level_value' => $data['level_value'],
+            'level_sort' => $data['level_sort'],
+            'is_hot' => $data['is_hot']
+        ];
+
+        return (new \app\common\model\Area())->modify($data['id'],$updateArr);
     }
 }
