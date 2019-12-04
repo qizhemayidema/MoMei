24ab97b1204de1e555c1dcfdaf95d23a744061fb
qizhemayidema <790353029@qq.com>
2019-12-04 13:48:59 +0800

    更新影院影厅管理

--- app/cinemaAdmin/controller/Screen.php
+++ app/cinemaAdmin/controller/Screen.php
@@ -3,12 +3,114 @@ declare (strict_types = 1);
 
 namespace app\cinemaAdmin\controller;
 
+use app\common\tool\Session;
+use think\facade\Validate;
+use app\common\service\CinemaScreen as Service;
+use think\facade\View;
 use think\Request;
 
 class Screen extends Base
 {
     public function index()
     {
+        $user = (new Session())->getData();
 
+        $cinema_id = $user['group_code'];
+
+
+        $list = (new Service())->getList($cinema_id,15);
+
+        View::assign('list',$list);
+        return view();
+    }
+
+    public function add()
+    {
+        return view();
+    }
+
+    public function save(Request $request)
+    {
+        $post = $request->post();
+
+        $validate = Validate::rule([
+            'name|影厅名称' => 'require|max:32',
+            'seat_sum|座位数量' => 'require|integer|between:0,999',
+            'sort|排序' => 'require|between:0,999',
+            '__token__' => 'token',
+        ]);
+
+
+        try{
+
+           if (!$validate->check($post)) throw new \Exception($validate->getError());
+
+
+            $user = (new Session())->getData();
+
+            $post['cinema_id'] = $user['group_code'];
+
+            (new Service())->insert($post);
+
+        }catch (\Exception $e){
+            return json(['code'=>0,'msg'=>$e->getMessage()]);
+        }
+
+        return json(['code'=>1,'msg'=>'success']);
+    }
+
+    public function edit(Request $request)
+    {
+        $id = $request->param('id');
+
+        $data = (new Service())->get($id);
+
+        View::assign('data',$data);
+
+        return view();
+    }
+
+    public function update(Request $request)
+    {
+        $post = $request->post();
+
+        $validate = Validate::rule([
+            'id'           => 'require',
+            'name|影厅名称' => 'require|max:32',
+            'seat_sum|座位数量' => 'require|integer|between:0,999',
+            'sort|排序' => 'require|between:0,999',
+            '__token__' => 'token',
+        ]);
+
+
+        try{
+
+            if (!$validate->check($post)) throw new \Exception($validate->getError());
+
+
+            $user = (new Session())->getData();
+
+            $post['cinema_id'] = $user['group_code'];
+
+            (new Service())->update($post['id'],$post);
+
+        }catch (\Exception $e){
+            return json(['code'=>0,'msg'=>$e->getMessage()]);
+        }
+
+        return json(['code'=>1,'msg'=>'success']);
+    }
+
+    public function delete(Request $request)
+    {
+        $id = $request->param('id');
+
+        $user = (new Session())->getData();
+
+        $cinemaId = $user['group_code'];
+
+        (new Service())->delete($id,$cinemaId);
+
+        return json(['code'=>1,'msg'=>'success']);
     }
 }
---
+++ app/common/service/CinemaScreen.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘彪
+ * Date: 2019/12/4
+ * Time: 13:07
+ */
+
+namespace app\common\service;
+
+
+class CinemaScreen
+{
+    public function getList($cinema_id,$page = null)
+    {
+        $handler = (new \app\common\model\CinemaScreen())->backgroundShowData()->where(['cinema_id'=>$cinema_id])
+            ->order('sort','desc');
+
+        return $page ? $handler->paginate($page) : $handler->select();
+    }
+
+    public function get($id)
+    {
+        return (new \app\common\model\CinemaScreen())->find($id);
+    }
+
+    public function insert($data)
+    {
+        $insert = [
+            'cinema_id' => $data['cinema_id'],
+            'name'      => $data['name'],
+            'seat_sum'  => $data['seat_sum'],
+            'sort'      => $data['sort'],
+            'create_time' => time(),
+        ];
+
+        $model = (new \app\common\model\CinemaScreen());
+
+        $model->insert($insert);
+
+        return $model->getLastInsID();
+    }
+
+    public function update($id,$data)
+    {
+        $update = [
+            'name'      => $data['name'],
+            'seat_sum'  => $data['seat_sum'],
+            'sort'      => $data['sort'],
+        ];
+
+        $model = (new \app\common\model\CinemaScreen());
+
+        $model->where(['id'=>$id,'cinema_id'=>$data['cinema_id']])->update($update);
+    }
+
+    public function delete($id,$cinemaId)
+    {
+        (new \app\common\model\CinemaScreen())->where(['id'=>$id,'cinema_id'=>$cinemaId])->update([
+            'delete_time' => time(),
+        ]);
+    }
+}
